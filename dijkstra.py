6b5cdfdbd48141d720ed8c888c8adc906f3e892d
social-anthrax <40488664+social-anthrax@users.noreply.github.com>
2019-09-06 10:01:05 +0000

    half way through complete, push so home

--- dijkstra.py
+++ dijkstra.py
@@ -43,6 +43,24 @@ for edge in edges:
 
 print(nodes)
 
+def dijsktra(nodes, initial, final_destination):
+    #shortest_paths is a dictionary of nodes
+    # where the value is a tuple of previos node, and 1
+    shortest_paths = {initial: (None, 0)}
+    current_noodle = initial
+    visited = set() #using a set to make sure i havent visited the node already
+
+    while  current_noodle != final_destination:
+        visited.add(current_noodle) #adds the current node to make sure we do not go back to it by accident
+        destinations = nodes.edges[current_noodle]
+        weight_to_current_noodle = shortest_paths[current_noodle][1]
+
+        for next_noodle in destinations:
+            weight = 1 + weight_to_current_noodle 
+            if next_noodle not in shortest_paths:
+                shortest_paths[next_noodle] = (current_noodle, weight)
+            else:
+                
 
 
 
