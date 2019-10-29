1db4d9da0249b922698c0a5dbfe15f981d3d9e2b
social-anthrax <40488664+social-anthrax@users.noreply.github.com>
2019-10-29 14:56:05 +0000

    managed to show prove concept

--- scraper.py
+++ scraper.py
@@ -1,4 +1,8 @@
 import scrapy
+import json
+from scrapy.crawler import CrawlerProcess
+
+import scrapy
 from scrapy.crawler import CrawlerProcess
 
 
@@ -9,36 +13,45 @@ class ScraperWithLimit(scrapy.Spider):
     ]
 
     custom_settings = {
-        'DEPTH_LIMIT': 2
+        'DEPTH_LIMIT': 1
     }
 
     def parse(self, response):
-        for next_page in response.css('div.mw-parser-output > p > a'):
+        file = open('yeet.txt', 'a')
+        for next_page in response.css('div.mw-parser-output > p >       a'):
             yield response.follow(next_page, self.parse)
 
-        for quote in response.css('div.mw-parser-output > p'):
-            yield {'quote': quote.extract()}
-
+        file.write(response.url + "\n")
+        # for quote in response.css('div.mw-parser-output > p'):
+            # yield {'quote': quote.extract()}
 
 class ScraperWithDuplicateRequests(scrapy.Spider):
+    
+
     name = "ScraperWithDuplicateRequests"
     start_urls = [
         'https://en.wikipedia.org/wiki/Web_scraping',
     ]
-
     custom_settings = {
         'DEPTH_LIMIT': 2
     }
 
     def parse(self, response):
+        
         for next_page in response.css('div.mw-parser-output > p > a::attr(href)').extract_first():
             if next_page is not None:
                 next_page = response.urljoin(next_page)
-                yield scrapy.Request(next_page, callback=self.parse, dont_filter=False)
-
+               
+                yield scrapy.Request(next_page, callback=self.parse, dont_filter=True)
+        
         for quote in response.css('div.mw-parser-output > p'):
-            yield {'quote': quote.extract()}
-
+            quote = quote.extract()
+            
+            yield {'quote': quote}
+        
+        
 process = CrawlerProcess()
-process.crawl(ScraperWithDuplicateRequests)
+process.crawl(ScraperWithLimit)
 process.start()
+process.stop()
+
