de9dfd4ee5da37647a64d446d14a79f44c7fc0e6
social-anthrax <40488664+social-anthrax@users.noreply.github.com>
2019-10-30 11:53:53 +0000

    now stays on domain

--- scraper.py
+++ scraper.py
@@ -9,21 +9,25 @@ from scrapy.crawler import CrawlerProcess
 class ScraperWithLimit(scrapy.Spider):
     name = "ScraperWithLimit"
     start_urls = [
-        'https://en.wikipedia.org/wiki/Web_scraping',
+        # 'https://en.wikipedia.org/wiki/Web_scraping',
+        # 'https://en.wikipedia.org/wiki/Pok%C3%A9mon',
+        # 'https://www.george-heriots.com/',
+        'https://www.sqa.org.uk/sqa/70972.html',
     ]
 
+    allowed_domains = ['www.sqa.org.uk']
+
     custom_settings = {
-        'DEPTH_LIMIT': 1
+        'DEPTH_LIMIT': 1,
+        'DEPTH_PRIORITY': -1,
     }
 
     def parse(self, response):
-        file = open('yeet.txt', 'a')
-        for next_page in response.css('div.mw-parser-output > p >       a'):
+        for next_page in response.css('a::attr(href)'):
             yield response.follow(next_page, self.parse)
 
         file.write(response.url + "\n")
-        # for quote in response.css('div.mw-parser-output > p'):
-            # yield {'quote': quote.extract()}
+        
 
 class ScraperWithDuplicateRequests(scrapy.Spider):
     
@@ -32,13 +36,15 @@ class ScraperWithDuplicateRequests(scrapy.Spider):
     start_urls = [
         'https://en.wikipedia.org/wiki/Web_scraping',
     ]
+    
     custom_settings = {
+        'DEPTH_PRIORITY': 1,
         'DEPTH_LIMIT': 2
     }
 
     def parse(self, response):
         
-        for next_page in response.css('div.mw-parser-output > p > a::attr(href)').extract_first():
+        for next_page in response.css('p > a::attr(href)').extract_first():
             if next_page is not None:
                 next_page = response.urljoin(next_page)
                
@@ -50,8 +56,11 @@ class ScraperWithDuplicateRequests(scrapy.Spider):
             yield {'quote': quote}
         
         
+file = open('yeet.txt', 'a')
 process = CrawlerProcess()
 process.crawl(ScraperWithLimit)
 process.start()
+
 process.stop()
+file.close()
 
