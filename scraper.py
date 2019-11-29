516a04e2245ad71dbe9fa63088d34115bf3beabc
social-anthrax <social-anthrax@users.noreply.github.com>
2019-11-29 15:48:38 +0000

    implemented writing to the database

--- scraper.py
+++ scraper.py
@@ -1,13 +1,16 @@
 
 # from scrapy.spiders import CrawlSpider, Rule
 # from scrapy.linkextractors import LinkExtractor
+import mysql.connector
 from collections import defaultdict
 import scrapy
 from scrapy.crawler import CrawlerProcess
 
 
-website = "https://www.sqa.org.uk/sqa/70972.html"
-domain = "www.sqa.org.uk"
+
+
+website = ""
+domain = ""
 dictOfUrl = defaultdict(list)
 if __name__ == "__main__": #checks if its being run as a module or as a standalone
     print("Script must be run as module")
@@ -38,29 +41,70 @@ class ScraperWithLimit(scrapy.Spider):
 
         # file.write(response.url + "\n")
 
-def runScrape(page = ""): #like runescape but not
-    if page == "":
+
+def runScrape(page="", jumps = 0):  # like runescape but not
+
+    
+    if jumps == 0: #checks if the max number of jumps has been modified.
+        jumps = input("Please input the max number of jumps to be performed by the scraper \n")
+    else:
+        jumps = 5
+
+    if page == "": #checks if the page has been passed as a parameter and if it hasnt then 
         website = input("Please input the website you wish to scrape: ")
     else:
         website = page
 
-    ScraperWithLimit.allowed_domains = [website.replace("https://", "").replace("http://", "").split("/", 1)[0]] #trims away anything making it a url, making it into a domain
+    domain = website.replace("https://", "").replace("http://", "").split("/", 1)[0]
+
+    ScraperWithLimit.allowed_domains = [domain] #trims away anything making it a url, making it into a domain
     ScraperWithLimit.start_urls = [website]
+    ScraperWithLimit.custom_settings = {
+        'DEPTH_LIMIT': jumps,
+        'DEPTH_PRIORITY': 0,
+    }
     process = CrawlerProcess()
     process.crawl(ScraperWithLimit)
     process.start()
     process.stop()
+    #region database setup
+    mydb = mysql.connector.connect(
+        host="localhost",
+        user="test",
+        passwd="test"
+    )
+    mycursor = mydb.cursor()
+    #endregion
 
-    file = open('yeet.txt', 'w')
-    for x, y in dictOfUrl.items():
-        file.write(x+", {")
-        for item in y:
-            # item = item[52:-1]
+    for originURL, hyperlinks  in dictOfUrl.items():
+        for item in hyperlinks:
             if item != "" and item != "/":
-                if item[0] == "/" or "http" in item:
+                if item[0] == "/" or "http" in item: #ignores all anchor links
+
                     if "http" not in item:
-                        file.write(item.replace('//', '') + ", ")
+                        mycursor.execute(
+                            "INSERT INTO %s (OriginURL, Hyperlink) VALUES (%s, %s);" % domain, originURL, item.replace('//', ''))
+                        
                     else:
-                        file.write(item + ", ")
-        file.write("}\n")
-    file.close()
+                        mycursor.execute(
+                            "INSERT INTO %s (OriginURL, Hyperlink) VALUES (%s, %s);" % domain, originURL, domain+item)  # appends the domain name to relative paths
+        
+    mycursor.close()
+    mydb.close()
+ 
+
+
+    #legacy code for when output was to a file
+    # file = open('yeet.txt', 'w') 
+    # for x, y in dictOfUrl.items():
+    #     file.write(x+", {")
+    #     for item in y:
+    #         if item != "" and item != "/":
+    #             if item[0] == "/" or "http" in item:
+    #                 if "http" not in item:
+    #                     file.write(item.replace('//', '') + ", ")
+    #                 else:
+    #                     file.write(item + ", ")
+    #     file.write("}\n")
+    # file.close()
+
