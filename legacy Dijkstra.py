43945511422c0cec10a9a569d0ac529dbd3aeac3
social-anthrax <social-anthrax@users.noreply.github.com>
2020-03-15 18:25:58 +0000

    added legacy files for proof of iteration.

---
+++ legacy Dijkstra.py
@@ -0,0 +1,175 @@
+# Proof of itteration
+
+from collections import defaultdict
+# import scraper.py
+
+
+class noodlemap():
+    #region declaration
+        def __init__(self):
+            self.__edges = defaultdict(list)
+            self.__Matrix = [[0 for y in range(0, 1)]
+                           for x in range(0, 1)]  # this initialises the 2d array as private, as python naming convention states the a double underscore is a private variable.
+
+            #this is a dictionary of all possible NEXT noodles.
+    #endregion
+    #region setters
+        def __add_edge(self, origin_noodle, destination_noodle):
+
+            #adds new nodes witht the nodes they lead to. The path is assumed to be in one direction.
+
+            self.__edges[origin_noodle].append(destination_noodle)
+
+        def load(self, filename):    
+            lines = open(filename, 'r').readlines()
+
+            #as in python variables are hard typed, this is declaring a 2d array populated entirely by zeros
+
+            cols_count = 2
+            rows_count = len(lines)
+            self.__Matrix = [[0 for x in range(cols_count)]
+                                               for y in range(rows_count)]
+
+            i_d1 = 0 #index of dimension 1
+            i_d2 = 0  # index of dimension 2
+            for singleLine in lines:
+                singleLine = singleLine.replace(" ", "") #makes sure that there is no unnecessary spaces in the csv
+                i_d2 = 0
+                for y in singleLine.strip().split(','):  # splits up the two arguments and removes any new line characters.
+                    self.__Matrix[i_d1][i_d2] = y
+                    i_d2 += 1
+                i_d1 += 1
+
+            for index in range(0, rows_count):
+
+                self.__add_edge(self.__Matrix[index][0], self.__Matrix[index][1])  # adds to the dictionary of edges
+
+    #endregion
+    #region getters
+        def dijsktra(self, initial, final_destination):
+            #shortest_paths is a dictionary of noodles
+            # where the value is a tuple of previos noodle, and 1
+            shortest_paths = {initial: (None, 0)}
+            current_noodle = initial
+            visited = set()  # using a set to make sure i havent visited the node already
+
+            while current_noodle != final_destination:
+                visited.add(current_noodle)  # adds the current node to make sure we do not go back to it by accident
+                destinations = noodles.__edges[current_noodle]
+
+                for next_noodles in destinations:
+                    if next_noodles not in shortest_paths:  # checks if node has been passed yet
+                        shortest_paths[next_noodles] = (current_noodle, 1)
+                    else:
+                        current_shortest_weight = shortest_paths[next_noodles][1]
+                        if current_shortest_weight > 1:
+                            shortest_paths[next_noodles] = (current_noodle, 1)
+
+
+                possible_noodle = defaultdict(list)  # initialises an empty dictionary
+                for noodle in shortest_paths:
+                    if noodle not in visited:
+                        possible_noodle[noodle] = shortest_paths[noodle]
+
+
+                if not possible_noodle:  # if the next possible nodes is empty
+                    return "No route can be found from %s to %s" % (initial, final_destination)
+                current_noodle = min(possible_noodle, key=lambda k: possible_noodle[k][1])  # this part goes back through the destinations on shortest_paths 
+
+            path = []  # initialises an list/array
+            while current_noodle is not None:
+                path.append(current_noodle)
+                next_noodles = shortest_paths[current_noodle][0]
+                current_noodle = next_noodles
+            #itterates through the path list with a step of -1, aka backwards
+            path = path[::-1]
+            return path
+
+        def returnMap(self):
+            unsortedlist = list(self.__edges)  # creates an "array" of the keys of the __edges dictionary.
+
+            for startvalue in range(1, len(unsortedlist)):
+
+                for currentValue in range(startvalue, 0, -1):
+                    for letter in range(0 , min(len(unsortedlist[currentValue]), len(unsortedlist[currentValue - 1]))): #finds the lowest length of the two urls and loops for that amount so to not go over the limit.
+                        if ord(((unsortedlist[currentValue])[letter]).lower()) < ord(((unsortedlist[currentValue-1])[letter]).lower()):  # if the letters are the same then the next letter is selected so that they are still alphabetical.
+                            tempLower = unsortedlist[currentValue]
+                            tempHigher = unsortedlist[currentValue - 1]
+                            unsortedlist[currentValue - 1] = tempLower
+                            unsortedlist[currentValue] = tempHigher
+                            break
+
+                        elif len(unsortedlist[currentValue]) < len(unsortedlist[currentValue - 1]) and ord(((unsortedlist[currentValue])[letter]).lower()) == ord(((unsortedlist[currentValue-1])[letter]).lower()):  # if the values are equal and the length is different it sorts them into the correct order of shortest first for readability
+                            tempLower = unsortedlist[currentValue]
+                            tempHigher = unsortedlist[currentValue - 1]  # remove one of these as this is unnecessary
+                            unsortedlist[currentValue - 1] = tempLower
+                            unsortedlist[currentValue] = tempHigher
+
+            sortedList = defaultdict(list)
+            for key in unsortedlist:
+                sortedList[key] = self.__edges[key]
+            return sortedList
+
+    #endregion
+
+class ui():
+    #region setters
+    def __init__(self, section):
+        self.sectionName = section
+        self.__contents = ""
+        self.__prompt = ""
+        self.__commands = defaultdict(list)
+
+    # the kwargs is used to get a dictionary of functions that can be called by typing in the key. Instructions on how to do so were found here: https://stackoverflow.com/questions/9205081/is-there-a-way-to-store-a-function-in-a-list-or-dictionary-so-that-when-the-inde
+    # use of kwargs was helped by https://www.digitalocean.com/community/tutorials/how-to-use-args-and-kwargs-in-python-3
+    def setContents(self, contentsText):
+        self.__contents = contentsText
+
+    def setCommands(self, prompt, **kwargs):
+        self.__prompt = prompt
+        for key, value in kwargs.items():
+            self.__commands[key] = value
+
+    #endregion
+
+    #region getters
+    def showUi(self, acceptCommands=True):
+        print(self.__contents)
+        if acceptCommands == True:
+            userInput = input(self.__prompt + "   ")
+            if userInput in self.__commands:
+                self.__commands[userInput]()
+            else:
+                print("Please select a valid option.")
+
+#the proceedures bellow simplify the proccesses
+
+def scrape():
+    import subprocess
+    subprocess.Popen("scraper.py", shell=True)
+
+
+def pathfinder():
+    start = input(
+        "Please input The webpage you wish the path to begin with. \n")
+    end = input("Please input the webpage you wish the path to terminate at. \n")
+    noodles.load('map.csv')
+    print(noodles.dijsktra(start, end)) 
+
+
+def sort():
+    noodles.load('map.csv')
+    print(noodles.returnMap())
+
+
+def help():
+    print("do this")
+
+
+noodles = noodlemap()
+mainMenu = ui("MainMenu")  # instanciates UI object
+mainMenu.setContents(
+        'Welcome to PathFinder! To see help, type: help \n Options: \n pathfinder: Finds a path between two urls \n sort. Url link finder.')
+mainMenu.setCommands('Pick option', pathfinder=pathfinder,
+                     sort=sort, help=help)
+mainMenu.showUi()
---
+++ legacy scraper.py
@@ -0,0 +1,116 @@
+# proof of iteration
+
+# from scrapy.spiders import CrawlSpider, Rule
+# from scrapy.linkextractors import LinkExtractor
+import mysql.connector
+from collections import defaultdict
+import scrapy
+from scrapy.crawler import CrawlerProcess
+
+
+website = ""
+domain = ""
+dictOfUrl = defaultdict(list)
+if __name__ == "__main__":  # checks if its being run as a module or as a standalone
+    print("Script must be run as module")
+    exit()
+
+
+class ScraperWithLimit(scrapy.Spider):
+    name = "ScraperWithLimit"
+    start_urls = [
+        #these are all test case websites with known outputs
+        # 'https://en.wikipedia.org/wiki/Web_scraping',
+        # 'https://en.wikipedia.org/wiki/Pok%C3%A9mon',
+        # 'https://www.sqa.org.uk/sqa/70972.html',
+    ]
+
+    allowed_domains = [domain]
+
+    custom_settings = {
+        'DEPTH_LIMIT': 1,
+        'DEPTH_PRIORITY': 0,
+    }
+
+    def parse(self, response):
+        for next_page in response.css('a::attr(href)'):
+            # 56 is the first character at which the actual url starts and the -3 cuts off the last 3 characters of the string: eg <Selector xpath='descendant-or-self::a/@href' data='http://www.mysqa.org.uk/'> becomes http://www.mysqa.org.uk/
+            dictOfUrl[response.url].append(str(next_page.root))
+            # file.write(str(next_page)[55:-1] + '\n')
+            yield response.follow(next_page, self.parse)
+
+        # file.write(response.url + "\n")
+
+
+def runScrape(page="", jumps=0):  # like runescape but not
+
+    if jumps == 0:  # checks if the max number of jumps has been modified.
+        jumps = input(
+            "Please input the max number of jumps to be performed by the scraper \n")
+    else:
+        jumps = 5
+
+    if page == "":  # checks if the page has been passed as a parameter and if it hasnt then
+        website = input("Please input the website you wish to scrape: ")
+    else:
+        website = page
+
+    domain = website.replace(
+        "https://", "").replace("http://", "").split("/", 1)[0]
+
+    # trims away anything making it a url, making it into a domain
+    ScraperWithLimit.allowed_domains = [domain]
+    ScraperWithLimit.start_urls = [website]
+    ScraperWithLimit.custom_settings = {
+        'DEPTH_LIMIT': jumps,
+        'DEPTH_PRIORITY': 0,
+    }
+    process = CrawlerProcess()
+    process.crawl(ScraperWithLimit)
+    process.start()
+    process.stop()
+    #region database setup
+    mydb = mysql.connector.connect(
+        host="localhost",
+        user="test",
+        password="test",
+        database='websites',
+        auth_plugin='mysql_native_password'
+    )
+    mycursor = mydb.cursor()
+    # drops the table if it already exists
+    mycursor.execute("DROP TABLE IF EXISTS `%s`;" % domain)
+    mycursor.execute(  # creates a table with the name of the domain being scraped.
+        "CREATE TABLE `%s`(AutoID INT NOT NULL AUTO_INCREMENT PRIMARY KEY, OriginURL VARCHAR(300) NOT NULL, Hyperlink VARCHAR(300) NOT NULL)" % domain)
+
+    #endregion
+
+    for originURL, hyperlinks in dictOfUrl.items():
+        for item in hyperlinks:
+            if item != "" and item != "/":
+                if item[0] == "/" or "http" in item:  # ignores all anchor links
+
+                    if "http" not in item:
+                        mycursor.execute(
+                            "INSERT INTO `%s` (OriginURL, Hyperlink) VALUES (`%s`, `%s`);" % (domain, originURL, item.replace('//', '')))  # mysql.connector.errors.ProgrammingError: 1054 (42S22): Unknown column 'https://www.sqa.org.uk/sqa/70972.html' in 'field list'
+
+                    else:
+                        mycursor.execute(
+                            "INSERT INTO `%s` (OriginURL, Hyperlink) VALUES (`%s`, `%s`);" % (domain, originURL, (domain+item)))  # appends the domain name to relative paths
+
+    mycursor.close()
+    mydb.close()
+
+    #legacy code for when output was to a file
+    # file = open('yeet.txt', 'w')
+    # for x, y in dictOfUrl.items():
+    #     file.write(x+", {")
+    #     for item in y:
+    #         if item != "" and item != "/":
+    #             if item[0] == "/" or "http" in item:
+    #                 if "http" not in item:
+    #                     file.write(item.replace('//', '') + ", ")
+    #                 else:
+    #                     file.write(item + ", ")
+    #     file.write("}\n")
+    # file.close()
